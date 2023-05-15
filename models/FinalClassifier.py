6d2975982a3d5dbabce446bc5f426f9bf84ade85
Valerio Firmano <valerio.firmano@gmail.com>
2023-05-15 13:39:39 +0200

    -First development commit of TA3N - modified temporal aggregation function

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -18,6 +18,20 @@ class Classifier(nn.Module):
 
         self.TRN=RelationModuleMultiScale(1024, 1024, self.num_clips)
 
+        def temporal_aggregation(self, x):
+
+            x=x.view(-1, self.num_clips, 1024) #restore the original shape of the tensor
+
+            if self.avg_modality == 'Pooling':
+                x = self.AvgPool(x)
+                x = x.view(-1, 1024)
+            elif self.avg_modality == 'TRN':
+                x = self.TRN(x)
+                x = x.sum(1)
+            return x
+
+        self.temporal_aggregation = temporal_aggregation
+
         self.fc1 = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(1024, 1024),
@@ -30,15 +44,62 @@ class Classifier(nn.Module):
 
         #self.softmax = nn.Softmax(dim=1)
 
+    def forward(self, input_source, input_target, beta, is_train):
+        batch_source = input_source.size()[0]
+		batch_target = input_target.size()[0]
+        num_segments = self.num_clips
+        num_class = self.num_classes
+
+        feat_all_source = []
+		feat_all_target = []
+		pred_domain_all_source = []
+		pred_domain_all_target = []
+
+        # Here we reshape dimensions as batch x num_clip x feat_dim --> (batch * num_clip) x feat_dim because we
+        # want to process every clip independently not as part of a batch of clips that refers to the same video because
+        # we are at a frame level#
+        feat_base_source = input_source.view(-1, input_source.size()[-1]) # e.g. 32 x 5 x 1024 --> 160 x 1024
+		feat_base_target = input_target.view(-1, input_target.size()[-1]) # e.g. 32 x 5 x 1024 --> 160 x 1024
+
+        #Adaptive Batch Normalization and shared layers to ask, at the moment we put:
+        feat_fc_source = feat_base_source
+        feat_fc_target = feat_base_target
+
+        # === adversarial branch (frame-level), in our case clip-level ===#
+		pred_fc_domain_frame_source = self.domain_classifier_frame(feat_fc_source, beta) # 160 x 1024 --> 2
+		pred_fc_domain_frame_target = self.domain_classifier_frame(feat_fc_target, beta) # 160 x 1024 --> 2
+
+        # Da capire un attimo le dimensioni di questo append !!!
+        pred_domain_all_source.append(pred_fc_domain_frame_source.view((batch_source, num_segments) + pred_fc_domain_frame_source.size()[-1:]))
+		pred_domain_all_target.append(pred_fc_domain_frame_target.view((batch_target, num_segments) + pred_fc_domain_frame_target.size()[-1:]))
+
+        #=== prediction (frame-level) ===#
+		pred_fc_source = self.fc_classifier_source(feat_fc_source) # 160 x 1024 --> num_classes
+		pred_fc_target = self.fc_classifier_target(feat_fc_target) # 160 x 1024 --> num_classes
+
+        ### aggregate the frame-based features to relation-based features ###
+		feat_fc_relation_source = self.temporal_aggregation(feat_fc_source)
+		feat_fc_relation_target = self.temporal_aggregation(feat_fc_target)
+
+        # Here if we have used AvgPool we obtain a tensor of size batch x feat_dim, otherwise if we have used TRN we obtain
+        # a tensor of size batch x num_relations x feat_dim and we have to implement a domain classifier for the TRN case
+        # so i think we need to do something as:
+        # if self.avg_modality == 'TRN':
+        #  pred_fc_domain_relation_source = self.domain_classifier_relation(feat_fc_relation_source, beta)
+        #  pred_fc_domain_relation_target = self.domain_classifier_relation(feat_fc_relation_target, beta)
+        # where domain_classifier_relation have to be implemented as fc where num_relation x feat_dim -> 2
+        # pay attention that aggregated data with AvgPool are yet at video level (1x1024) so doing a relation domain classifier
+        # on them is useful because we will do it later so we have to put
+        # elif self.avg_modality == 'Pooling':
+        # feat_fc_video_source = feat_fc_relation_source
+        # feat_fc_video_target = feat_fc_relation_target
+
+
+
+
     def forward(self, x):
 
-        if self.avg_modality == 'Pooling':
-            x = self.AvgPool(x)
-            x = x.view(-1, 1024)
-
-        if self.avg_modality == 'TRN':
-            x = self.TRN(x)
-            x = x.sum(1)
+        x = self.temporal_aggregation(self,x)
 
         x = self.fc1(x)
         x= self.fc2(x)
